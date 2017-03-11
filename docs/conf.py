fb53a407bccf00a3fce6f513c22836c3ccee94b3
adierebel <adie.rebel@gmail.com>
2017-03-11 12:45:17 +0700

    fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
-import sys, os
+#import sys, os
 
-sys.path.insert(0, os.path.abspath('../'))
+#sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- General configuration -----------------------------------------------------
